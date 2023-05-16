f7c929da79dfc547de4f2e031a9acd07a27b498f
Kimo <128164209+Karim7Ramadan@users.noreply.github.com>
2023-05-16 13:38:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -75,20 +75,4 @@ function startCount(el) {
     }
   }, 2000 / goal);
 }
-let btn = document.querySelector(`.btn`);
 
-window.onscroll = function () {
-  if (window.scrollY >= 600) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-};
-
-btn.onclick = function () {
-  window.scrollTo({
-    left: 0,
-    top: 0,
-    behavior: "smooth",
-  });
-};
